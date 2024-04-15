7141b5fca362846ae7f13355b71647b8cd70c838
Henry Brink <henry@henrybrink.de>
2024-04-15 13:43:45 +0200

    Remove the users password from the response
    Signed-off-by: Henry Brink <henry@henrybrink.de>

--- backend/src/api/user/user.controller.ts
+++ backend/src/api/user/user.controller.ts
@@ -1,21 +1,30 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { AutoGuard } from 'src/auth/auto.guard';
+
+type SanatizedUser = Omit<User, 'password'>;
 
 @Controller('user')
 export class UserController {
 
-    /**
-     * /user/me
-     * 
-     * Returns information about the current User
-     * 
-     * @param req 
-     * @returns 
-     */
-    @Get("/me")
-    @UseGuards(AutoGuard)
-    async me(@Request() req) {
-        return req.user;
-    }
+  _sanatizeUser(user: User): SanatizedUser {
+    const sanatizedUser: SanatizedUser & { password?: string } = user;
 
+    delete sanatizedUser.password;
+    return sanatizedUser;
+  }
+
+  /**
+   * /user/me
+   *
+   * Returns information about the current User
+   *
+   * @param req
+   * @returns
+   */
+  @Get('/me')
+  @UseGuards(AutoGuard)
+  async me(@Request() req) {
+    return this._sanatizeUser(req.user);
+  }
 }
