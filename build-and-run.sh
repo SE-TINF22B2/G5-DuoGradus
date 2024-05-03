f572df0c826765889b9368bb63c44c0f04f3adba
Henry Brink <henry@henrybrink.de>
2024-05-03 10:34:14 +0200

    Auto deployment: Simple action (#155)
    * add auto deployment script
    
    * make the script more reliant

---
+++ build-and-run.sh
@@ -0,0 +1,13 @@
+# Stop the current docker compose server
+docker compose down
+
+# Download the current code main branch from github
+git fetch --all
+# Yes, this is the desired option!
+git reset --hard origin/master
+
+# Rebuild the images with the new code
+docker compose build
+
+# Restart the service
+docker compose up -d
