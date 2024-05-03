78c827411543e581d9bfc35c6c09776a0f28842f
Henry Brink <henry@henrybrink.de>
2024-05-03 10:24:06 +0200

    make the script more reliant

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,8 +1,10 @@
-# Download the current code main branch from github
-git pull
-
 # Stop the current docker compose server
 docker compose down
+
+# Download the current code main branch from github
+git fetch --all
+# Yes, this is the desired option!
+git reset --hard origin/master
 
 # Rebuild the images with the new code
 docker compose build
