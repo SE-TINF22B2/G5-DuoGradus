dfc394e71b4d10b8892e6ccc78630e966abcdf33
wwwLuis <luis.bernhardt@web.de>
2024-05-10 11:00:55 +0200

    fix: footer wird nurnoch bei den gewünschten seiten angezeigt

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  hasFooter() {
+    const footerPages: string[] = ['/main', '/ranking'];
+    return footerPages.includes(window.location.pathname);
+  }
 }
