7f9423d4cabde9a1863d5eea87cb852fc0b6aac8
Benedict Weis <47110382+benedictweis@users.noreply.github.com>
2024-05-15 08:25:30 +0200

    Fix Frontend Action (#181)
    * adjusted frontend action
    
    * fixed test
    
    * added ci test configuration

--- frontend/src/app/components/pages/profilepage/profilepage.component.spec.ts
+++ frontend/src/app/components/pages/profilepage/profilepage.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfilepageComponent } from './profilepage.component';
+import { StatisticEntryComponent } from 'app/components/atoms/statistic-entry/statistic-entry.component';
 
 describe('ProfilepageComponent', () => {
   let component: ProfilepageComponent;
@@ -8,10 +9,10 @@ describe('ProfilepageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ProfilepageComponent]
+      declarations: [ProfilepageComponent, StatisticEntryComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(ProfilepageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
