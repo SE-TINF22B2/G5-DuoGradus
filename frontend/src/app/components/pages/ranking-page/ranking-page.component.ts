452c230688d4424e2ff5644068b0305cd1938fed
huber <hubertjustin01@gmail.com>
2024-05-29 20:36:13 +0200

    Change Colors

--- frontend/src/app/components/pages/ranking-page/ranking-page.component.ts
+++ frontend/src/app/components/pages/ranking-page/ranking-page.component.ts
@@ -17,6 +17,16 @@ export class RankingPageComponent {
     { name: 'Leon', placement: 7, winstreak: 3 },
     { name: 'Luna', placement: 8, winstreak: 2 },
     { name: 'Kira', placement: 9, winstreak: 1 },
-    { name: 'Luna', placement: 10, winstreak: 0 }
+    { name: 'Luna', placement: 10, winstreak: 0 },
+    { name: 'Kira', placement: 11, winstreak: 0 },
+    { name: 'Luna', placement: 12, winstreak: 0 },
+    { name: 'Kira', placement: 13, winstreak: 0 },
+    { name: 'Luna', placement: 14, winstreak: 0 },
+    { name: 'Kira', placement: 15, winstreak: 0 },
+    { name: 'Luna', placement: 16, winstreak: 0 },
+    { name: 'Kira', placement: 17, winstreak: 0 },
+    { name: 'Luna', placement: 18, winstreak: 0 },
+    { name: 'Kira', placement: 19, winstreak: 0 },
+    { name: 'Luna', placement: 20, winstreak: 0 },
   ];
 }
