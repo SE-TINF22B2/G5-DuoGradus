7fdd4d5ead31b19e8b98db519ed4a00edc4ab08b
Henry Brink <henry@henrybrink.de>
2024-06-19 14:26:50 +0200

    enh: Add new task for demo (#254)
    * fix: streak not incremented
    
    Signed-off-by: Henry Brink <henry@henrybrink.de>
    
    * enh: Add new task
    
    Signed-off-by: Henry Brink <henry@henrybrink.de>
    
    * fix: invalid id
    
    Signed-off-by: Henry Brink <henry@henrybrink.de>
    
    ---------
    
    Signed-off-by: Henry Brink <henry@henrybrink.de>

---
+++ backend/src/app/tasks/tasks/static/task4.ts
@@ -0,0 +1,18 @@
+import { StepTask } from '../task.step';
+import { TaskInfo } from '../task.types';
+
+export class Task4 extends StepTask {
+  getRequiredSteps(): number {
+    return 4000;
+  }
+  getInfo(): TaskInfo {
+    return {
+      id: '4',
+      title: 'Schritte-Meister: Junior',
+      description: 'Erreiche mindestens 4.000 Schritte',
+      points: 20,
+      conditions: [],
+      status: this.userStatus,
+    };
+  }
+}
